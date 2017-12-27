41f7cf65f2dec1d76a054fee48fc78a9300912c7
MakcBerluskone <Makc_berluskone@mail.ru>
2017-12-27 18:15:42 +0800

    Допилены штат и сотрудники, возможны правки

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -17,7 +17,7 @@
 	  <div class="top-bar head-content">
 	    <div class="top-bar-left">
 	      <h2 class="header-content">{{ $page_info->page_name }}</h2>
-	      <a href="/positions/create" class="icon-add sprite"></a>
+	      <a href="/staff" class="icon-add sprite"></a>
 	    </div>
 	    <div class="top-bar-right">
 	      <a class="icon-filter sprite"></a>
