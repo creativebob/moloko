73fa9e28e4161fe7e8b56b5c603f1a328f43bd13
MakcBerluskone <makc_berluskone@mail.ru>
2018-04-11 10:38:08 +0800

    Алиас навигации

--- database/seeds/NavigationsTableSeeder.php
+++ database/seeds/NavigationsTableSeeder.php
@@ -11,42 +11,40 @@ class NavigationsTableSeeder extends Seeder
      */
     public function run()
     {
-         DB::table('navigations')->insert([
-            [
-                'navigation_name' => 'Разделы управления сайтом',
-                'navigation_alias' => null,
-                'site_id' => null, 
-                'system_item' => 1,  
-                'company_id' => null,
-                'category_navigation_id' => 1,         
-            ],
-            [
-                'navigation_name' => 'Левый сайдбар',
-                'navigation_alias' => null,
-                'site_id' => 1, 
-                'system_item' => 1,  
-                'company_id' => null,
-                'category_navigation_id' => 2,         
-            ],
-            [
-                'navigation_name' => 'Главное меню',
-                'navigation_alias' => 'general',
-                'site_id' => 2,  
-                'system_item' => null, 
-                'company_id' => 1,
-                'category_navigation_id' => 2,            
-            ],
-            [
-                'navigation_name' => 'Меню продукции',
-                'navigation_alias' => 'products',
-                'site_id' => 2, 
-                'system_item' => null,  
-                'company_id' => 1,
-                'category_navigation_id' => 2,            
-            ],
-            
-           
+     DB::table('navigations')->insert([
+      [
+        'navigation_name' => 'Разделы управления сайтом',
+        'navigation_alias' => null,
+        'site_id' => null, 
+        'system_item' => 1,  
+        'company_id' => null,
+        'category_navigation_id' => 1,         
+      ],
+      [
+        'navigation_name' => 'Левый сайдбар',
+        'navigation_alias' => null,
+        'site_id' => 1, 
+        'system_item' => 1,  
+        'company_id' => null,
+        'category_navigation_id' => 2,         
+      ],
+      [
+        'navigation_name' => 'Главное меню',
+        'navigation_alias' => 'general',
+        'site_id' => 2,  
+        'system_item' => null, 
+        'company_id' => 1,
+        'category_navigation_id' => 2,            
+      ],
+      [
+        'navigation_name' => 'Меню продукции',
+        'navigation_alias' => 'products',
+        'site_id' => 2, 
+        'system_item' => null,  
+        'company_id' => 1,
+        'category_navigation_id' => 2,            
+      ],
 
-        ]);
-    }
-}
+    ]);
+   }
+ }
