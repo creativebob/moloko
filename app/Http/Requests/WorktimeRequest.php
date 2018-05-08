bd671da47b4f340033cf596a3b3381ed69815110
creativebob <creativebob@mail.ru>
2018-05-08 18:17:10 +0800

    Доделал Расписание

---
+++ app/Http/Requests/WorktimeRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class WorktimeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+
+            'mon_begin' => 'regex:[0-1][0-9]|[2][0-3]):[0-5][0-9]|max:5|nullable', 
+
+        ];
+    }
+}
