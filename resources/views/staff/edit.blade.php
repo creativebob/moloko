c374cdcf7290c021ae9b033b27fc6a43916c72c2
MakcBerluskone <makc_berluskone@mail.ru>
2018-05-11 17:58:38 +0800

    Правки

--- resources/views/staff/edit.blade.php
+++ resources/views/staff/edit.blade.php
@@ -6,7 +6,7 @@
 
 @section('title', 'Редактировать сотрудника')
 
-@section('breadcrumbs', Breadcrumbs::render('edit', $page_info, $staffer->position->position_name))
+@section('breadcrumbs', Breadcrumbs::render('edit', $page_info, $staffer->position->name))
 
 @section('title-content')
 	<div class="top-bar head-content">
