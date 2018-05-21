65efd4c9d300d9bd77797381dcd2c2fee841b80b
MakcBerluskone <makc_berluskone@mail.ru>
2018-05-21 14:42:10 +0800

    Страны

--- database/seeds/LocationsTableSeeder.php
+++ database/seeds/LocationsTableSeeder.php
@@ -12,41 +12,49 @@ class LocationsTableSeeder extends Seeder
 				'city_id' => 1,
 				'address' => 'ул. Шевцова, 5',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 1,
 				'address' => 'ул. Кекная, 5',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 1,
 				'address' => 'ул. Лольная, 5',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 2,
 				'address' => 'ул. Шевцова, 5',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 1,
 				'address' => 'ул. Шевцова, 5, оф. 202',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 2,
 				'address' => 'ул. Ленина, 45б',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 4,
 				'address' => 'ул. Мира, 11',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 			[
 				'city_id' => 1,
 				'address' => 'ул. Шевцова, 61',
 				'author_id' => 1,
+				'country_id' => 1,
 			],
 		]);
 	}
