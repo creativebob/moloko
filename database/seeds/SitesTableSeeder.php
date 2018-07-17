bcd012e24450f9ba8f4b90a9831305e58913a9f0
Makc_Berluskone <makc_berluskone@mail.ru>
2018-07-17 11:11:02 +0800

    Трабл с новостями

--- database/seeds/SitesTableSeeder.php
+++ database/seeds/SitesTableSeeder.php
@@ -23,16 +23,16 @@ class SitesTableSeeder extends Seeder
                 'api_token' => str_random(60),
         	],
 
-            [
-                'name' => 'vtoroy',
-                'domain' => 'vtoroy',
-                'alias' => 'vtoroy',
-                'company_id' => null,
-                'author_id' => 1,
-                'system_item' => 1,
-                'moderation' => null,
-                'api_token' => str_random(60),
-            ],
+            // [
+            //     'name' => 'vtoroy',
+            //     'domain' => 'vtoroy',
+            //     'alias' => 'vtoroy',
+            //     'company_id' => null,
+            //     'author_id' => 1,
+            //     'system_item' => 1,
+            //     'moderation' => null,
+            //     'api_token' => str_random(60),
+            // ],
            
         ]);
     }
