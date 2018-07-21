b3d889e7536166040810b7bcaacbcfe4810fe093
creativebob <creativebob@mail.ru>
2018-07-21 23:00:48 +0800

    Правки - привоим в порядок систему

--- resources/views/layouts/sidebar-list.blade.php
+++ resources/views/layouts/sidebar-list.blade.php
@@ -12,8 +12,8 @@
 @else
 	{{-- Если конечный пункт --}}
 	<li>
-		{{-- <a href="{{ $sidebar['alias'] }}" data-link="{{ $sidebar['id'] }}">{{ $sidebar['name'] }}</a> --}}
-		{{ link_to($sidebar['alias'], $title = $sidebar['name'], $attributes = ['data-link' => $sidebar['id']], $secure = null) }}
+		<a href="/..{{ $sidebar['alias'] }}" data-link="{{ $sidebar['id'] }}">{{ $sidebar['name'] }}</a>
+		{{-- link_to($sidebar['alias'], $title = $sidebar['name'], $attributes = ['data-link' => $sidebar['id']], $secure = null) --}}
 	</li>
 @endif
 
