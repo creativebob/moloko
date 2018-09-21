b032067f851ee1840b8a8853d52548ca45b930b2
creativebob <creativebob@mail.ru>
2018-09-21 17:55:39 +0800

    Исправлены некоторые баги в фильтре по периоду

--- app/Scopes/Filters/DateIntervalFilter.php
+++ app/Scopes/Filters/DateIntervalFilter.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Scopes\Filters;
+use Carbon\Carbon;
 
 trait DateIntervalFilter
 {
@@ -8,26 +9,25 @@ trait DateIntervalFilter
     public function scopeDateIntervalFilter($query, $request, $name_column)
     {
 
+        if(isset($request->date_start)){$date_start = Carbon::createFromFormat('d.m.Y H:i:s', $request->date_start . '00:00:00');};
+        if(isset($request->date_end)){$date_end = Carbon::createFromFormat('d.m.Y H:i:s', $request->date_end . '23:59:59');};
+
         //Фильтруем временному интервалу: если указана только начальная дата
         if(($request->date_start)&&(!isset($request->date_end))){
-            $date_start = date_to_mysql($request->date_start);
             $query = $query->where($name_column, '>', $date_start);
         };
 
         //Фильтруем временному интервалу: если указана только конечная дата
         if(($request->date_end)&&(!isset($request->date_start))){
-            $date_end = date_to_mysql($request->date_end);
             $query = $query->where($name_column, '<', $date_end);
         };
 
         //Фильтруем временному интервалу
         if(($request->date_start)&&($request->date_end)){
-      			$date_start = date_to_mysql($request->date_start);
-      			$date_end = date_to_mysql($request->date_end);
            	$query = $query->whereBetween($name_column, [$date_start, $date_end]);
         };
 
-      return $query;
+        return $query;
     }
 
 }
