b5c00db497e173e73c018d68450a2aba9e003d37
Makc_Berluskone <makc_berluskone@mail.ru>
2018-10-10 15:46:57 +0800

    Правка новостей

--- app/EntityPage.php
+++ app/EntityPage.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class EntityPage extends Model
 {
     protected $table = 'entity_page';
+    protected $fillable = [
+        'entity_id', 
+        'page_id', 
+    ];
 }
