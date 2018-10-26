6f52e0624a67c1688a62f1d79364ef37563467aa
creativebob <creativebob@mail.ru>
2018-10-26 16:58:02 +0800

    Созданы контейнеры под виджеты

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,27 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Traits\Widgets\WidgetsTrait;
 
 class DashboardController extends Controller
 {
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // Подключаем построители виджетов
+    use WidgetsTrait;
 
     // Сущность над которой производит операции контроллер
     protected $entity_name = 'dashboard';
 
+    // Инициируем контейнер виджета
+    protected $widgets_total = [];
+
     public function index()
     {
+
+        // Генерируем виджеты
+        $this->addWidgets(['sales-department-burden', 'map-leads']);
+
+        $widgets = $this->widgets_total;
+
         // Инфо о странице
         $page_info = pageInfo($this->entity_name);
-        
-        
-        
-        return view('dashboard', compact('page_info'));
+
+        return view('dashboard', compact('page_info', 'widgets'));
     }
+
 }
---
+++ resources/views/includes/widgets/sales-department-burden.blade.php
@@ -0,0 +1,61 @@
+                        <div class="small-12 medium-12 cell">
+                            <div class="card">
+                                <div class="card-section">
+                                    <div class="grid-x grid-padding-x">
+                                        <div class="auto cell"><h3 class="widget-h3">Нагрузка на отдел продаж</h3></div>
+                                        <div class="shrink cell">
+                                            <div class="sprite icon-drop"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <table class="widget-table stack unstriped hover responsive-card-table">
+                                    <thead>
+                                        <tr>
+                                            <th rowspan="2" class="right-border">Менеджер</th>
+                                            <th colspan="4" class="common-th right-border">Лиды</th>
+                                            <th colspan="6" class="common-th">Кол-во задач</th>
+                                        </tr>
+                                        <tr>
+                                            <th>В работе</th>
+                                            <th>Без задач</th>
+                                            <th>Бюджет</th>
+                                            <th class="right-border">Отказы</th>
+                                            <th>Все</th>
+                                            <th>Просрочка</th>
+                                            <th>Сегодня</th>
+                                            <th>Завтра</th>
+                                            <th>Неделя</th>
+                                            <th>Отложенные</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td class="right-border" data-label="Менеджер">Виноградова Алена</td> 
+                                            <td data-label="Лидов в работе">{{ num_format(1500, 0) }}</td>
+                                            <td data-label="Лидов без задач">{{ num_format(500, 0) }}</td>
+                                            <td data-label="Бюджет">{{ num_format(1462300, 0) }}</td>
+                                            <td data-label="Отказы" class="right-border">{{ num_format(2, 0) }}</td>
+                                            <td data-label="Все задачи">{{ num_format(68, 0) }}</td>
+                                            <td data-label="Просроченные">{{ num_format(20, 0) }} <span class="tiny-text">({{ num_format(32.5, 0) }}%)</span></td>
+                                            <td data-label="Задачи на сегодня">{{ num_format(47, 0) }}</td>
+                                            <td data-label="Задачи на завтра">{{ num_format(78, 0) }}</td>
+                                            <td data-label="Задачи на неделю">{{ num_format(151, 0) }}</td>
+                                            <td data-label="Отложенные">{{ num_format(151, 0) }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="right-border" data-label="Менеджер">Полянская Екатерина</td> 
+                                            <td data-label="Лидов в работе">{{ num_format(2500, 0) }}</td>
+                                            <td data-label="Лидов без задач">{{ num_format(780, 0) }}</td>
+                                            <td data-label="Бюджет">{{ num_format(1462300, 0) }}</td>
+                                            <td data-label="Отказы" class="right-border">{{ num_format(0, 0) }}</td>
+                                            <td data-label="Все задачи">{{ num_format(34, 0) }}</td>
+                                            <td data-label="Просроченные">{{ num_format(8, 0) }}</td>
+                                            <td data-label="Задачи на сегодня">{{ num_format(57, 0) }}</td>
+                                            <td data-label="Задачи на завтра">{{ num_format(18, 0) }}</td>
+                                            <td data-label="Задачи на неделю">{{ num_format(121, 0) }}</td>
+                                            <td data-label="Отложенные">{{ num_format(151, 0) }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
