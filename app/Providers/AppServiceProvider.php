43a2d0f750149e96c15ddac64896ca4d5eff8655
Makc_Berluskone <makc_berluskone@mail.ru>
2018-12-24 09:43:57 +0800

    Умолчания фоток

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,20 +30,21 @@ class AppServiceProvider extends ServiceProvider
             config()->set('photo_settings', PhotoSetting::whereNull('company_id')->first()->toArray());
         } else {
             // Умолчания на случай, если нет доступа к базе (Для формирования autoload)
+            $settings = [];
             config()->set('photo_settings', [
-                $settings->img_small_width = 0,
-                $settings->img_small_height = 0,
-                $settings->img_medium_width = 0,
-                $settings->img_medium_height = 0,
-                $settings->img_large_width = 0,
-                $settings->img_large_height = 0,
+                $settings['img_small_width'] = 0,
+                $settings['img_small_height'] = 0,
+                $settings['img_medium_width'] = 0,
+                $settings['img_medium_height'] = 0,
+                $settings['img_large_width'] = 0,
+                $settings['img_large_height'] = 0,
 
-                $settings->img_formats = 0,
-                $settings->imgstrict_mode = 0,
+                $settings['img_formats'] = 0,
+                $settings['imgstrict_mode'] = 0,
 
-                $settings->img_min_width = 0,
-                $settings->img_min_height = 0,
-                $settings->img_max_size = 0,
+                $settings['img_min_width'] = 0,
+                $settings['img_min_height'] = 0,
+                $settings['img_max_size'] = 0,
             ]);
 
         }
