5db6150c3bd01df51e3c6e6d31dfba691d76381b
creativebob <creativebob@mail.ru>
2019-01-14 11:41:24 +0800

    Сделана правка шаблона создания метрик

--- resources/views/includes/metrics_category/properties_list.blade.php
+++ resources/views/includes/metrics_category/properties_list.blade.php
@@ -1,16 +1,20 @@
-<ul class="checker" id="{{ $set_status }}-properties-list">
-	@foreach ($properties as $property)
-	@if($property->metrics->isNotEmpty())
-	@include('includes.metrics_category.property', ['property' => $property, 'set_status' => $set_status])
-	@endif
-	@endforeach
 
-	{{-- @each('products.property', $properties, 'property') --}}
+<div class="grid-x grid-padding-x">
+	<div class="small-12 cell">
+		<ul class="checker" id="{{ $set_status }}-properties-list">
+			@foreach ($properties as $property)
+			@if($property->metrics->isNotEmpty())
+			@include('includes.metrics_category.property', ['property' => $property, 'set_status' => $set_status])
+			@endif
+			@endforeach
 
-	<li>
-		<br>
+			{{-- @each('products.property', $properties, 'property') --}}
+		</ul>
+	</div>
+
+	<div class="small-12 cell wrap-add-new-metric">
 		<label>Создать свойство
 			{{ Form::select('property_id', $properties->pluck('name', 'id'), null, ['id' => $set_status.'-properties-select', 'placeholder' => 'Выберите свойство']) }}
 		</label>
-	</li>
-</ul>
+	</div>
+</div>
