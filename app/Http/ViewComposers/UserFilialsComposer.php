40bae5c9bf20ff9eaa8906631ef3154afc8ba383
Makc Berluskone <makc_berluskone@mail.ru>
2019-06-28 13:27:58 +0800

    Филиалы пользователя на прайсах услуг

--- app/Http/ViewComposers/UserFilialsComposer.php
+++ app/Http/ViewComposers/UserFilialsComposer.php
@@ -11,16 +11,8 @@ class UserFilialsComposer
 	public function compose(View $view)
 	{
         $user_filials  = session('access.all_rights.index-prices_services-allow.filials');
-        // dd($user_filials);
 
-		$filials = Department::
-        // whereIn('id', array_keys($user_filials))
-        // ->
-        whereNull('parent_id')
-        ->toBase()
-        ->get();
-
-		return $view->with(compact('filials'));
+		return $view->with(compact('user_filials'));
 
 	}
 
--- resources/views/prices_services/select_user_filials.blade.php
+++ resources/views/prices_services/select_user_filials.blade.php
@@ -1,1 +1,1 @@
-{!! Form::select('filial_id', $filials->pluck('name', 'id'), $filial_id, ['id' => 'select-user_filials']) !!}
+{!! Form::select('filial_id', $user_filials, $filial_id, ['id' => 'select-user_filials']) !!}
