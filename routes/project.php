c5744438bac2c5674c84b09ba0419e25089f3537
Makc Berluskone <mahafaHASL2014git>
2019-08-16 16:38:47 +0800

    Правка маршрутов каталогов для сайта

--- routes/project.php
+++ routes/project.php
@@ -14,7 +14,7 @@
 // Первый запуск
 Route::get('/', 'AppController@start')->name('project.start');
 
-Route::get('/catalogs-goods/{catalog_slug}/{catalog_item_slug}', 'AppController@catalog_goods')->name('project.catalog_goods');
-Route::get('/catalogs-services/{catalog_slug}/{catalog_item_slug}', 'AppController@catalog_services')->name('project.catalog_services');
+Route::get('/catalogs-goods/{catalog_slug}/{catalog_item_slug}', 'AppController@catalogs_goods')->name('project.catalogs_goods');
+Route::get('/catalogs-services/{catalog_slug}/{catalog_item_slug}', 'AppController@catalogs_services')->name('project.catalogs_services');
 
 Route::get('/price-goods/{id}/', 'AppController@price_goods')->name('project.price_goods');
