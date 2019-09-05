25c12eaab0b1e2be2afd0a0fd6f3b6a3047d4085
creativebob <creativebob@mail.ru>
2019-09-05 15:36:25 +0800

    Фикс бага с фильтром (step)

--- public_html/js/system/app.js
+++ public_html/js/system/app.js
@@ -38130,8 +38130,8 @@ __webpack_require__(36);
 
 // Наши скрипты
 __webpack_require__(62);
-__webpack_require__(64);
 __webpack_require__(63);
+__webpack_require__(64);
 
 /***/ }),
 /* 39 */
@@ -57223,44 +57223,6 @@ window.submitAjax = function (id) {
 /* 63 */
 /***/ (function(module, exports) {
 
-function checkFilter() {
-
-    // var marginTop = '6.2em';
-    // if ($('#thead-sticky').hasClass('is-stuck')) {
-    // 	var string = $('#thead-sticky').css('marginTop');
-    if ($('.icon-filter').hasClass('active-filter')) {
-        $('#filters').css('display', 'block');
-
-        $('#thead-sticky').css('marginTop', '15em');
-        $('#thead-sticky').attr('data-margin-top', 15);
-    } else {
-        $('#filters').css('display', 'none');
-        $('#thead-sticky').attr('data-margin-top', 6.2);
-        $('#thead-sticky').css('marginTop', '6.2em');
-    };
-    // };
-};
-
-// Блок фильтра
-$(document).on('click', '.icon-filter', function () {
-    $(this).toggleClass("active-filter");
-    checkFilter();
-});
-// $('.icon-filter').click(function() {
-
-// });
-
-$(document).on('click', '.filter-close', function () {
-    $('.icon-filter').removeClass("active-filter");
-    $('#filters').css('display', 'none');
-    $('#thead-sticky').attr('data-margin-top', 6.2);
-    $('#thead-sticky').css('marginTop', '6.2em');
-});
-
-/***/ }),
-/* 64 */
-/***/ (function(module, exports) {
-
 // Умолчания глобальные
 // Время применения изменений
 var transitionTime = 200;
@@ -57769,6 +57731,44 @@ $(window).resize(function () {
 // $('.icon-footer').bind('click', function() {
 //   $('#foot-drop').toggleClass('active-foot-drop');
 // });
+
+/***/ }),
+/* 64 */
+/***/ (function(module, exports) {
+
+function checkFilter() {
+
+    // var marginTop = '6.2em';
+    // if ($('#thead-sticky').hasClass('is-stuck')) {
+    // 	var string = $('#thead-sticky').css('marginTop');
+    if ($('.icon-filter').hasClass('active-filter')) {
+        $('#filters').css('display', 'block');
+
+        $('#thead-sticky').css('marginTop', '15em');
+        $('#thead-sticky').attr('data-margin-top', 15);
+    } else {
+        $('#filters').css('display', 'none');
+        $('#thead-sticky').attr('data-margin-top', 6.2);
+        $('#thead-sticky').css('marginTop', '6.2em');
+    };
+    // };
+};
+
+// Блок фильтра
+$(document).on('click', '.icon-filter', function () {
+    $(this).toggleClass("active-filter");
+    checkFilter();
+});
+// $('.icon-filter').click(function() {
+
+// });
+
+$(document).on('click', '.filter-close', function () {
+    $('.icon-filter').removeClass("active-filter");
+    $('#filters').css('display', 'none');
+    $('#thead-sticky').attr('data-margin-top', 6.2);
+    $('#thead-sticky').css('marginTop', '6.2em');
+});
 
 /***/ }),
 /* 65 */
