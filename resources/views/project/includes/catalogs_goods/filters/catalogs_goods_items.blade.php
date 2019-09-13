77fbbd14d576b07e9fd290f1e5a95d1005b9921a
Makc Berluskone <makc_berluskone@mail.ru>
2019-09-13 16:50:25 +0800

    Update catalogs_goods_items.blade.php

--- resources/views/project/includes/catalogs_goods/filters/catalogs_goods_items.blade.php
+++ resources/views/project/includes/catalogs_goods/filters/catalogs_goods_items.blade.php
@@ -2,6 +2,6 @@
 	<div class="cell small-12">
 
 		<label><h4>Пункты</h4>
-			{!! Form::select('catalogs_goods_item', $catalogs_goods_items->pluck('name', 'id'), $catalog_goods->id) !!}
+			{!! Form::select('catalogs_goods_item', $catalogs_goods_items->pluck('name', 'id'), $catalog_goods_item->id, ['placeholder' => 'Везде']) !!}
 		</label>
 </div>
