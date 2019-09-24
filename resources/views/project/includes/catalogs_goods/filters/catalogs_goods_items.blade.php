4cce15a58b3054455237c4095235c6c875d11cb0
creativebob <creativebob@mail.ru>
2019-09-24 16:38:58 +0800

    Фикс бага с селектом фильтра Позиции прайса на project

--- resources/views/project/includes/catalogs_goods/filters/catalogs_goods_items.blade.php
+++ resources/views/project/includes/catalogs_goods/filters/catalogs_goods_items.blade.php
@@ -1,7 +1,7 @@
 <div class="grid-x">
 	<div class="cell small-12">
 		<label><h4>Категории:</h4>
-			{!! Form::select('catalogs_goods_item', $catalogs_goods_items, null, ['placeholder' => 'Везде', 'class'=>'catalogs_goods_item_filter']) !!}
+			{!! Form::select('catalogs_goods_item', $catalogs_goods_items, request()->catalogs_goods_item ?? null, ['placeholder' => 'Все категории', 'class'=>'catalogs_goods_item_filter']) !!}
 		</label>
 	</div>
 </div>
