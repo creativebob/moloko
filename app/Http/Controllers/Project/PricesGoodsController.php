4794a4fb1a26ebfbc1623d4613b99e8d454ef585
creativebob <mbcb13git>
2019-11-01 14:23:59 +0800

    Update PricesGoodsController.php

--- app/Http/Controllers/Project/PricesGoodsController.php
+++ app/Http/Controllers/Project/PricesGoodsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Project;
 
-App\Models\Project\PricesGoods;
+use App\Models\Project\PricesGoods;
 use App\Site;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
