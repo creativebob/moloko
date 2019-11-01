9a6e56ce75e8eff93a9e5c3932ae30cb26fbd2dc
creativebob <mbcb13git>
2019-11-01 13:38:08 +0800

    Update project.php

--- routes/project.php
+++ routes/project.php
@@ -24,16 +24,32 @@ Route::resource('/prices-goods', 'PricesGoodsController')
     ->only(['show'])
     ->names('project.prices_goods');
 
+Route::any('/update_cookies', 'CartController@update_cookies')->name('project.update_cookies');
+
+Route::resource('/cart', 'CartController')
+    ->only(['index', 'store', 'update_cookies'])
+    ->names('project.carts');
+
 Route::resource('/estimates', 'EstimateController')
     ->only(['index', 'show'])
     ->names('project.estimates');
 
-Route::any('/update_cookies', 'CartController@update_cookies')->name('project.update_cookies');
-Route::resource('/cart', 'CartController')
-    ->only(['index', 'store', 'update_cookies'])
-    ->names('project.carts');
-//Route::post('/cart_store', 'AppController@cart_store')->name('project.cart_store');
+Route::get('/goods-composition/{id}/', 'AppController@goods_composition')->name('project.goods_composition');
 
+Route::post('/site_user_login', 'AppController@site_user_login')->name('project.site_user_login');
+Route::get('/confirmation', 'AppController@confirmation')->name('project.confirmation');
+
+// Генерация access_code и отправка его на телефон пользователя
+Route::post('/get_access_code', 'AppController@get_access_code')->name('project.get_access_code');
+Route::post('/get_sms_code', 'AppController@get_sms_code')->name('project.get_sms_code');
+// Route::post('/login_by_access_code', 'AppController@login_by_access_code')->name('project.login_by_access_code');
 Route::get('/telegram', 'AppController@telegram')->name('project.telegram');
 
 Route::post('/delivery_update', 'AppController@delivery_update')->name('project.delivery_update');
+
+Route::get('logout_siteuser', 'AppController@logout_siteuser')->name('project.logout_siteuser');
+
+Route::get('/cabinet', 'AppController@cabinet')->name('project.cabinet')->middleware('auth_usersite');
+Route::post('/update_profile', 'AppController@update_profile')->name('project.update_profile');
+
+Route::get('/{page_alias}', 'AppController@dynamic_pages')->name('project.dynamic_pages');
