c3bcccb4ff82f9b03aed0aab2ce4aea0b82d5b15
Makc_Berluskone <makc_berluskone@mail.ru>
2019-12-09 22:10:14 +0800

    Update PromotionsComposer.php

--- app/Http/ViewComposers/Project/PromotionsComposer.php
+++ app/Http/ViewComposers/Project/PromotionsComposer.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\ViewComposers\Project;
 
-use App\Promotion;
-use Illuminate\Support\Facades\Cookie;
 use Illuminate\View\View;
 
 class PromotionsComposer
@@ -12,16 +10,12 @@ class PromotionsComposer
     public function compose(View $view)
     {
 
-        $promotions = Promotion::where([
-            'filial_id' => $view->site->filial->id,
-            'display' => true
-        ])
+        $site = $view->site->load('filial.promotions');
+        $promotions = $site->filial->promotions->where('filial_id', 1)
             ->where('begin_date', '<', now())
             ->where('end_date', '>', now())
-            ->orderBy('sort')
-            ->get();
-//        $site = $view->site->load('filials.promotions');
-//        $promotions = $site->filials->first()->promotions->where('filial_id', 1)->where('begin_date', '<', now())->where('end_date', '>', now())->where('display', true)->sortBy('sort');
+            ->where('display', true)
+            ->sortBy('sort');
 
         return $view->with(compact('promotions'));
     }
