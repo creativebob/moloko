bf7597514c168072a91ab66b13c71625e8331e19
Makc_Berluskone <makc_berluskone@mail.ru>
2019-12-09 22:47:14 +0800

    worktime

--- app/Http/ViewComposers/Project/WorktimeFilialTodayComposer.php
+++ app/Http/ViewComposers/Project/WorktimeFilialTodayComposer.php
@@ -8,7 +8,15 @@ class WorktimeFilialTodayComposer
     public function compose(View $view)
     {
         $worktimes = $view->site->filial->worktime;
-        return $view->with(compact('worktimes'));
+        if ($worktimes[date('N')]['begin'] && $worktimes[date('N')]['begin']) {
+            $worktime = $worktimes[date('N')]['begin'] . ' - ' . $worktimes[date('N')]['end'];
+        } else {
+            $worktime = 'ВЫходной';
+        }
+        return $view->with(compact('worktime'));
+
+//        $worktimes = $view->site->filial->worktime;
+//        return $view->with(compact('worktimes'));
     }
 
 }
