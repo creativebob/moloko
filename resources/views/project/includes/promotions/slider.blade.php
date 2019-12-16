0c3572cd8a9983f6621d42e32972e4184276caa8
Makc_Berluskone <makc_berluskone@mail.ru>
2019-12-16 20:48:33 +0800

    Update slider.blade.php

--- resources/views/project/includes/promotions/slider.blade.php
+++ resources/views/project/includes/promotions/slider.blade.php
@@ -1,64 +1,102 @@
-                
-                <aside class="cell aside-actions small-12">
-                    {{-- <div class="line">
-                      <span class="actions-text"><span class="flame"></span>Акции</span>
-                    </div> --}}
-                    <div class="grid-x contur">
-                        <div class="cell small-12 block-actions single-item">
-                            @foreach($promotions as $promotion)
-                              <div class="item-slide">
-                                  <a href="{{ $promotion->link }}">
-                                    <picture>
-                                        <source media="(max-width: 420px)" srcset="{{ $promotion->tiny->path }} 420w" sizes="100vw">
-                                        <source media="(max-width: 800px)" srcset="{{ $promotion->small->path }} 800w" sizes="100vw">
-                                        <source media="(max-width: 1024px)" srcset="{{ $promotion->medium->path }} 1024w" sizes="100vw">
-                                        <source media="(max-width: 1280px)" srcset="{{ $promotion->large->path }} 1280w" sizes="100vw">
-                                        <source media="(max-width: 2000px)" srcset="{{ $promotion->large_x->path }} 2000w" sizes="200vw">
-                                        <img srcset="{{ $promotion->medium->path }} 1024w">
-                                    </picture>
-                                  </a>
-                              </div>
-                            @endforeach
-                        </div>
+@if($promotions->count() > 0)
+    <aside class="cell aside-actions small-12">
+        <div class="grid-x contur wrap-slick">
+            <div class="cell small-12 block-actions single-item">
+                @foreach($promotions as $promotion)
+                    <div class="item-slide">
+                        <a href="{{ $promotion->link }}">
+                            <picture>
+                                <source media="(max-width: 420px)" srcset="{{ $promotion->tiny->path }} 420w" sizes="100vw">
+                                <source media="(max-width: 800px)" srcset="{{ $promotion->small->path }} 800w" sizes="100vw">
+                                <source media="(max-width: 1024px)" srcset="{{ $promotion->medium->path }} 1024w" sizes="100vw">
+                                <source media="(max-width: 1280px)" srcset="{{ $promotion->large->path }} 1280w" sizes="100vw">
+                                <source media="(max-width: 2000px)" srcset="{{ $promotion->large_x->path }} 2000w" sizes="200vw">
+                                <img srcset="{{ $promotion->medium->path }} 1024w">
+                            </picture>
+                        </a>
                     </div>
-                </aside>
-                @push('scripts')
-                    <script type="application/javascript">
-                        $(".single-item").slick({
+                @endforeach
+            </div>
+        </div>
+        <div class="line grid-x">
+            <div class="cell small-12">
+                <div id="hider-promotions">
+                         <span class="actions-text">
+                         </span>
+                </div>
+            </div>
+        </div>
+    </aside>
+@endif
 
-                      // normal options...
-                      infinite: false,
-                      // arrows: false,
 
-                      // the magic
-                      responsive: [{
+@push('scripts')
+    <script type="application/javascript">
 
-                          breakpoint: 2000,
-                          settings: {
-                            dots: true
-                          }
+        let promotion = window.localStorage.getItem('promotions') ?  window.localStorage.getItem('promotions') : window.localStorage.setItem('promotions', 'open');
 
-                        }, {
+        if (promotion == 'open') {
+            $('.wrap-slick').removeClass('hide-slide').removeClass('show-slide');
+            $('.wrap-slick').addClass('show-slide');
 
-                          breakpoint: 1024,
-                          settings: {
-                            infinite: true,
-                            dots: true
-                          }
+            $('#hider-promotions').removeClass('hider-on');
+        } else {
+            $('.wrap-slick').removeClass('hide-slide').removeClass('show-slide');
+            $('.wrap-slick').addClass('hide-slide');
 
-                        }, {
+            $('#hider-promotions').addClass('hider-on');
+        }
 
-                          breakpoint: 600,
-                          settings: {
-                            dots: true
-                          }
+        $(document).on('click', '#hider-promotions', function() {
+            $('.wrap-slick').toggleClass('hide-slide show-slide');
 
-                        }, {
+            if ($('.wrap-slick').hasClass('hide-slide')) {
+                window.localStorage.setItem('promotions', 'close');
+            } else {
+                window.localStorage.setItem('promotions', 'open');
+            }
 
-                          breakpoint: 300,
-                          settings: "unslick" // destroys slick
+            $('#hider-promotions').toggleClass('hider-on');
 
-                        }]
-                    });
-                    </script>
-                @endpush
+            // Запустить движение слайдов
+            // $('.single-item').slick('slickPlay');
+        });
+
+        $(".single-item").slick({
+
+            // normal options...
+            infinite: false,
+            // arrows: false,
+
+            // the magic
+            responsive: [{
+
+                breakpoint: 2000,
+                settings: {
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 1024,
+                settings: {
+                    infinite: true,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+                settings: "unslick" // destroys slick
+
+            }]
+        });
+    </script>
+@endpush
