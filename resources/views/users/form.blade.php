94cb88172bb9e6f57d0ac684ec25e569eba3b56c
lana lana <creativebob@mail.ru>
2019-12-16 22:46:29 +0800

    Ограничение на шаблоне пользователя - доступ к созданию ролей

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -434,30 +434,32 @@
 			<div class="grid-x grid-padding-x">
 
 				@if (isset($user->login))
-				<div class="small-12 cell tabs-margin-top">
-					<table class="content-table">
-						<caption>Уровень доступа</caption>
-						<thead>
-							<tr>
-								<th>Роль</th>
-								<th>Филиал</th>
-								<th>Должность</th>
-								<th>Инфа</th>
-								<th class="td-delete"></th>
-							</tr>
-						</thead>
-						<tbody class="roleuser-table">
-							@if (!empty($user->role_user))
-								@foreach ($user->role_user as $role_user)
-									@include('users.roles', $role_user)
-								@endforeach
-							@endif
-						</tbody>
-					</table>
-				</div>
-				<div class="small-8 small-offset-2 medium-8 medium-offset-2 tabs-margin-top text-center cell">
-					<a class="button" data-open="role-add">Настройка доступа</a>
-				</div>
+					@can('index', App\Role::class)
+						<div class="small-12 cell tabs-margin-top">
+							<table class="content-table">
+								<caption>Уровень доступа</caption>
+								<thead>
+									<tr>
+										<th>Роль</th>
+										<th>Филиал</th>
+										<th>Должность</th>
+										<th>Инфа</th>
+										<th class="td-delete"></th>
+									</tr>
+								</thead>
+								<tbody class="roleuser-table">
+									@if (!empty($user->role_user))
+										@foreach ($user->role_user as $role_user)
+											@include('users.roles', $role_user)
+										@endforeach
+									@endif
+								</tbody>
+							</table>
+						</div>
+						<div class="small-8 small-offset-2 medium-8 medium-offset-2 tabs-margin-top text-center cell">
+							<a class="button" data-open="role-add">Настройка доступа</a>
+						</div>
+					@endcan
 				@endif
 
 				<div class="small-12 text-center cell checkbox">
