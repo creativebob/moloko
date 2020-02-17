e605559b3942a97fa85101b85eac641276f59300
lana lana <creativebob@mail.ru>
2020-02-17 22:12:19 +0800

    Изменения в сиде Сервисов аккаунта и Ресурсов

--- resources/views/catalogs_goods_items/seo.blade.php
+++ resources/views/catalogs_goods_items/seo.blade.php
@@ -7,101 +7,65 @@
 
 @section('title', $page_info->name)
 
-@section('breadcrumbs', Breadcrumbs::render('index', $page_info))
-
 @section('content-count')
 {{-- Количество элементов --}}
-  @if(!empty($accounts))
-    {{ num_format($accounts->total(), 0) }}
+  @if(!empty($catalogs_goods_items))
+    {{ num_format($catalogs_goods_items->count(), 0) }}
   @endif
 @endsection
 
 @section('title-content')
 {{-- Таблица --}}
-@include('includes.title-content', ['page_info' => $page_info, 'class' => App\Account::class, 'type' => 'table'])
+@php $page_info->title = 'SEO Description: ' . $catalog_goods->name; @endphp
+@include('includes.title-content', ['page_info' => $page_info, 'class' => null, 'type' => 'table'])
 @endsection
 
 @section('content')
 {{-- Таблица --}}
 <div class="grid-x">
   <div class="small-12 cell">
-    <table class="content-table tablesorter" id="content" data-sticky-container data-entity-alias="accounts">
+    <table class="content-table tablesorter" id="content" data-sticky-container data-entity-alias="catalogs_goods_items">
       <thead class="thead-width sticky sticky-topbar" id="thead-sticky" data-sticky data-margin-top="6.2" data-sticky-on="medium" data-top-anchor="head-content:bottom">
         <tr id="thead-content">
           <th class="td-drop"></th>
           <th class="td-checkbox checkbox-th"><input type="checkbox" class="table-check-all" name="" id="check-all"><label class="label-check" for="check-all"></label></th>
-          <th class="td-name" data-serversort="name">Рабочее имя аккаунта</th>
-          <th class="td-description">Описание</th>
-          <th class="td-source">Сервис</th>
-          <th class="td-login">Логин</th>
-          <th class="td-alias">Алиас</th>
-          <th class="td-control"></th>
-          <th class="td-delete"></th>
+          <th class="td-name" data-serversort="name">Название пункта</th>
+          <th class="td-seo-description">Description</th>
+          <th class="td-keyword">Ключевые слова</th>
       </tr>
   </thead>
   <tbody data-tbodyId="1" class="tbody-width">
-    @if(!empty($accounts))
-    @foreach($accounts as $account)
-    <tr class="item @if($user->account_id == $account->id)active @endif  @if($account->moderation == 1)no-moderation @endif" id="accounts-{{ $account->id }}" data-name="{{ $account->name }}">
+    @if(!empty($catalogs_goods_items))
+    @foreach($catalogs_goods_items as $catalogs_goods_item)
+    <tr class="item @if($user->catalogs_goods_item_id == $catalogs_goods_item->id)active @endif  @if($catalogs_goods_item->moderation == 1)no-moderation @endif" id="catalogs_goods_items-{{ $catalogs_goods_item->id }}" data-name="{{ $catalogs_goods_item->name }}">
       <td class="td-drop"><div class="sprite icon-drop"></div></td>
       <td class="td-checkbox checkbox">
-        <input type="checkbox" class="table-check" name="account_id" id="check-{{ $account->id }}"
+        <input type="checkbox" class="table-check" name="catalogs_goods_item_id" id="check-{{ $catalogs_goods_item->id }}"
 
         {{-- Если в Booklist существует массив Default (отмеченные пользователем позиции на странице) --}}
         @if(!empty($filter['booklist']['booklists']['default']))
         {{-- Если в Booklist в массиве Default есть id-шник сущности, то отмечаем его как checked --}}
-        @if (in_array($account->id, $filter['booklist']['booklists']['default'])) checked
+        @if (in_array($catalogs_goods_item->id, $filter['booklist']['booklists']['default'])) checked
         @endif
         @endif
-        ><label class="label-check" for="check-{{ $account->id }}"></label>
+        ><label class="label-check" for="check-{{ $catalogs_goods_item->id }}"></label>
     </td>
     <td class="td-name">
-        @php
-        $edit = 0;
-        @endphp
-        @can('update', $account)
-        @php
-        $edit = 1;
-        @endphp
-        @endcan
-        @if($edit == 1)
-        <a href="/admin/accounts/{{ $account->id }}/edit">
-          @endif
-          {{ $account->name }}
-          @if($edit == 1)
+        <a href="/admin/catalogs_goods/{{$catalog_goods->id}}/catalogs_goods_items/{{ $catalogs_goods_item->id }}/edit">
+          {{ $catalogs_goods_item->name }}
       </a>
-      @endif
-  </td>
-  <td class="td-description">{{ $account->description }} </td>
-  <td class="td-source">{{ $account->source_service->name }}</td>
-  <td class="td-login">{{ $account->login }}</td>
-  <td class="td-alias">{{ $account->alias }}</td>
+    </td>
+  <td class="td-seo-description">{{ $catalogs_goods_item->seo_description }} </td>
+  <td></td>
 
-  {{-- Элементы управления --}}
-  @include('includes.control.table-td', ['item' => $account])
-
-  <td class="td-delete">
-    @if ($account->system != 1)
-    @can('delete', $account)
-    <a class="icon-delete sprite" data-open="item-delete"></a>
-    @endcan
-    @endif
-  </td>
-</tr>
-@endforeach
+  </tr>
+  @endforeach
 @endif
 </tbody>
 </table>
 </div>
 </div>
 
-{{-- Pagination --}}
-<div class="grid-x" id="pagination">
-  <div class="small-6 cell pagination-head">
-    <span class="pagination-title">Кол-во записей: {{ $accounts->count() }}</span>
-    {{ $accounts->appends(isset($filter['inputs']) ? $filter['inputs'] : null)->links() }}
-</div>
-</div>
 @endsection
 
 @section('modals')
