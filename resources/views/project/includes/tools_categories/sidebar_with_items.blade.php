64db9bdfbe1cb13689def635abb8c079044f90f2
Makc_Berluskone <makc_berluskone@mail.ru>
2020-02-19 12:39:29 +0800

    Update sidebar_with_items.blade.php

--- resources/views/project/includes/tools_categories/sidebar_with_items.blade.php
+++ resources/views/project/includes/tools_categories/sidebar_with_items.blade.php
@@ -1,1 +1,1 @@
-@include($site->alias . '.pages.partials.sidebars.tools_categories.sidebar', ['tools_categories' => buildTree($tools_categories)])
+@include($site->alias . '.pages.partials.sidebars.tools_categories.sidebar')
