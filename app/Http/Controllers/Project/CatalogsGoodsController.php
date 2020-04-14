211fa2a9924d9d1a0f72591f564ecd131bf95831
Makc_Berluskone <makc_berluskone@mail.ru>
2020-04-14 12:45:11 +0800

    Update CatalogsGoodsController.php

--- app/Http/Controllers/Project/CatalogsGoodsController.php
+++ app/Http/Controllers/Project/CatalogsGoodsController.php
@@ -50,40 +50,25 @@ class CatalogsGoodsController extends Controller
      * @param  string  $url
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request, $url)
+    public function show($slug)
     {
-
-         // TODO - 30.10.19 - Костыль по парсингу роута для вложенных пунктов каталога, нужно нормальное решение
-
-//        dd(__METHOD__, $url);
-        $arr = explode('/', $url);
-
-        $catalog_slug = $arr[0];
-        $main_slug = $arr[1] ?? null;
-        $sub_menu_ids = null;
-
-        if (count($arr) > 1) {
-            $sliced = array_slice($arr, 1);
-            $slug = '';
-            foreach($sliced as $slice) {
-                $slug .= $slice . '/';
-            }
-
-            $catalog_item_slug = substr($slug, 0, -1);
-
-        } else {
-
-            $catalog_item_slug = null;
-        }
-
         $site = $this->site;
-
         $page = $site->pages_public->where('alias', 'catalogs-goods')->first();
 
         // Получаем полный прайс со всеми доступными разделами
         $catalog_goods = CatalogsGoods::with([
-            'items_public' => function ($q) {
+            'items_public' => function ($q) use ($site) {
                 $q->with([
+                    'prices_public' => function ($q) use ($site) {
+                        $q->has('goods_public')
+                            ->where([
+                                'display' => true,
+                                'archive' => false,
+                                'filial_id' => $this->site->filial->id
+                            ])
+                            ->filter(request())
+                            ->orderBy('sort', 'asc');
+                    },
                     'display_mode',
                     'filters.values'
                 ]);
@@ -92,98 +77,100 @@ class CatalogsGoodsController extends Controller
             ->whereHas('filials', function ($q) use ($site) {
                 $q->where('id', $site->filial->id);
             })
-            ->where('slug', $catalog_slug)
+            ->where('slug', $slug)
             ->where(['display' => true])
             ->first();
+//        dd($catalog_goods);
 
-            // Проверим, а доступен ли каталог товаров. Если нет, то кидаем ошибку
-            if(!$catalog_goods){abort(403, 'Доступ к прайсу товаров компании ограничен. Согласен, это довольно странно...'); }
-            if($catalog_item_slug){
-
-            // Получаем разделы прайса ограниченный slug'ом
-            $catalog_goods_items = $catalog_goods->items_public->where('slug', $catalog_item_slug)->first();
-            // dd($catalog_goods_items);
-
-            if($catalog_goods_items){
-
-                $page->title = $catalog_goods_items->title;
-                $page->description = $catalog_goods_items->seo_description;
-
-                $catalog_goods_items->load('childs');
-                //dd($catalog_goods_items);
-
-                $sub_menu = $catalog_goods->items_public->where('slug', getFirstSlug($catalog_item_slug))->first();
-                $sub_menu->load('childs');
-                $sub_menu_ids = $sub_menu->childs->pluck('id');
-
-                // Получаем id всех доступных на сайте разделов прайса,
-                // чтобы далее не заниматься повторным перебором при получении товаров
-                $catalog_goods_items_ids = $catalog_goods->items_public->where('slug', $catalog_item_slug)->pluck('id');
-
-
-            } else {
-
-                abort(404, 'Страница не найдена');
-            }
-
-
+        // Проверим, а доступен ли каталог товаров. Если нет, то кидаем ошибку
+        if ($catalog_goods) {
+            return view($site->alias.'.pages.catalogs_goods.index', compact('site',  'page', 'catalog_goods'));
         } else {
-
-            // Получаем все доступные разделы прайса
-            $catalog_goods_items = $catalog_goods->items_public;
-            $page->title = 'Все товары';
-            $catalog_goods_items_ids = $catalog_goods->items_public->pluck('id');
+            abort(403, 'Доступ к прайсу товаров компании ограничен. Согласен, это довольно странно...');
         }
-
-
-        if($sub_menu_ids){
-            if(getFirstSlug($catalog_item_slug) == $catalog_item_slug){
-                $catalog_goods_items_ids = $catalog_goods_items_ids->merge($sub_menu_ids);
-            }
-        }
-
-        $prices_goods = PricesGoods::with([
-            'goods_public' => function ($q) {
-                $q->with([
-                    'article' => function ($q) {
-                        $q->with([
-                            'photo',
-                            'raws' => function ($q) {
-                                $q->with([
-                                    'article.unit',
-                                    'metrics'
-                                ]);
-                            },
-                            'attachments' => function ($q) {
-                                $q->with([
-                                    'article.unit',
-                                ]);
-                            },
-                            'containers' => function ($q) {
-                                $q->with([
-                                    'article.unit',
-                                ]);
-                            },
-                        ]);
-                    },
-                    'metrics',
-                ]);
-            },
-            'currency',
-            'catalogs_item.directive_category:id,alias'
-        ])
-            ->whereIn('catalogs_goods_item_id', $catalog_goods_items_ids)
-            ->has('goods_public')
-            ->where([
-                'display' => true,
-                'archive' => false,
-                'filial_id' => $this->site->filial->id
-            ])
-            ->filter(request())
-            ->orderBy('sort', 'asc')
-            ->paginate(16);
-
-        return view($site->alias.'.pages.catalogs_goods.index', compact('site',  'page', 'request', 'catalog_goods_items', 'prices_goods', 'catalog_goods', 'main_slug', 'sub_menu'));
+//            if($catalog_item_slug){
+//
+//            // Получаем разделы прайса ограниченный slug'ом
+//            $catalog_goods_items = $catalog_goods->items_public->where('slug', $catalog_item_slug)->first();
+//            // dd($catalog_goods_items);
+//
+//            if($catalog_goods_items){
+//
+//                $page->title = $catalog_goods_items->title;
+//                $page->description = $catalog_goods_items->seo_description;
+//
+//                $catalog_goods_items->load('childs');
+//                //dd($catalog_goods_items);
+//
+//                $sub_menu = $catalog_goods->items_public->where('slug', getFirstSlug($catalog_item_slug))->first();
+//                $sub_menu->load('childs');
+//                $sub_menu_ids = $sub_menu->childs->pluck('id');
+//
+//                // Получаем id всех доступных на сайте разделов прайса,
+//                // чтобы далее не заниматься повторным перебором при получении товаров
+//                $catalog_goods_items_ids = $catalog_goods->items_public->where('slug', $catalog_item_slug)->pluck('id');
+//
+//
+//            } else {
+//
+//                abort(404, 'Страница не найдена');
+//            }
+//
+//
+//        } else {
+//
+//            // Получаем все доступные разделы прайса
+//            $catalog_goods_items = $catalog_goods->items_public;
+//            $page->title = 'Все товары';
+//            $catalog_goods_items_ids = $catalog_goods->items_public->pluck('id');
+//        }
+//
+//        if($sub_menu_ids){
+//            if(getFirstSlug($catalog_item_slug) == $catalog_item_slug){
+//                $catalog_goods_items_ids = $catalog_goods_items_ids->merge($sub_menu_ids);
+//            }
+//        }
+//
+//        $prices_goods = PricesGoods::with([
+//            'goods_public' => function ($q) {
+//                $q->with([
+//                    'article' => function ($q) {
+//                        $q->with([
+//                            'photo',
+//                            'raws' => function ($q) {
+//                                $q->with([
+//                                    'article.unit',
+//                                    'metrics'
+//                                ]);
+//                            },
+//                            'attachments' => function ($q) {
+//                                $q->with([
+//                                    'article.unit',
+//                                ]);
+//                            },
+//                            'containers' => function ($q) {
+//                                $q->with([
+//                                    'article.unit',
+//                                ]);
+//                            },
+//                        ]);
+//                    },
+//                    'metrics',
+//                ]);
+//            },
+//            'currency',
+//            'catalogs_item.directive_category:id,alias'
+//        ])
+//            ->whereIn('catalogs_goods_item_id', $catalog_goods_items_ids)
+//            ->has('goods_public')
+//            ->where([
+//                'display' => true,
+//                'archive' => false,
+//                'filial_id' => $this->site->filial->id
+//            ])
+//            ->filter(request())
+//            ->orderBy('sort', 'asc')
+//            ->paginate(16);
     }
 
     /**
