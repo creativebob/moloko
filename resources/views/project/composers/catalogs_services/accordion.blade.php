019cf4d39554d0528fdc8f8ac68cb7226c68f683
Makc_Berluskone <makc_berluskone@mail.ru>
2020-04-16 13:13:10 +0800

    Шаблоны с получением данных

--- resources/views/project/composers/catalogs_services/accordion.blade.php
+++ resources/views/project/composers/catalogs_services/accordion.blade.php
@@ -4,7 +4,7 @@
 					@if(is_null($catalogs_services_item->parent_id))
 						<li><a href="/catalogs-services/{{ $catalogs_services_item->catalog->slug }}/{{ $catalogs_services_item->slug }}"><h3>{{ $catalogs_services_item->name }}</h3></a>
 							@isset ($catalogs_services_item->childrens)
-								@include('project.includes.catalogs_goods.sidebar_item', ['items' => $catalogs_services_item->childrens])
+								@include('project.composers.catalogs_goods.sidebar_item', ['items' => $catalogs_services_item->childrens])
 							@endisset
 						</li>
 					@endif
--- resources/views/project/composers/catalogs_services/sidebar_item.blade.php
+++ resources/views/project/composers/catalogs_services/sidebar_item.blade.php
@@ -3,7 +3,7 @@
 		<li>
 			<a href="/catalogs-seevices/{{ $item->catalog->slug }}/{{ $item->slug }}">{{ $item->name }}</a>
 			@isset ($item->childrens)
-				@include('project.includes.sidebar_item', ['items' => $item->childrens])
+				@include('project.composers.sidebar_item', ['items' => $item->childrens])
 			@endisset
 		</li>
 	@endforeach
