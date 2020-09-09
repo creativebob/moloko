111ed5c40549dfde04cf7b6ea6d3f10183664e4d
Makc_Berluskone <makc_berluskone@mail.ru>
2020-09-09 15:30:45 +0800

    Update 2020_03_06_141117_create_company_setting_table.php

--- database/migrations/2020_03_06_141117_create_company_setting_table.php
+++ database/migrations/2020_03_06_141117_create_company_setting_table.php
@@ -15,10 +15,10 @@ class CreateCompanySettingTable extends Migration
     {
         Schema::create('company_setting', function (Blueprint $table) {
             $table->bigInteger('company_id')->nullable()->unsigned()->comment('Id компании');
-            $table->foreign('company_id')->references('id')->on('companies');
+//            $table->foreign('company_id')->references('id')->on('companies');
 
             $table->bigInteger('setting_id')->nullable()->unsigned()->comment('Id настройки');
-            $table->foreign('setting_id')->references('id')->on('settings');
+//            $table->foreign('setting_id')->references('id')->on('settings');
         });
     }
 
