46dfb38d9552efbf8a0a499575c11dfea9934fcf
lana lana <creativebob@mail.ru>
2020-10-23 22:31:34 +0800

    Раскомент роута на тест рассылки

---
+++ resources/views/system/templates/emails/offers/newyear2020/index.blade.php
@@ -0,0 +1,228 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+  <head>
+    <link rel="stylesheet" type="text/css" href="vkusnyashka/emails/offers/newyear2021/css/app.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Вкусняшка - Магазин новогодних подарков!</title>
+    <!-- <style> -->
+  </head>
+  <body>
+    <span class="preheader"></span>
+    <table class="body">
+      <tr>
+        <td class="center" align="center" valign="top">
+          <center>
+            
+            <!-- <container class="header">
+              <row>
+                <columns>
+                  <spacer size="60"></spacer>
+                  <h1 class="text-center">Добро пожаловать в магазин Вкусняшка</h1>
+            
+                  <center>
+                    <menu class="text-center">
+                      <item href="https://вкусняшка-иркутск.рф/main">Магазин</item>
+                      <item href="https://вкусняшка-иркутск.рф/delivery">Доставка</item>
+                      <item href="https://вкусняшка-иркутск.рф/contacts">Контакты</item>
+                    </menu>
+                  </center>
+            
+                </columns>
+              </row>
+            </container> -->
+            
+            <table align="center" class="container body-border float-center"><tbody><tr><td>
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="32" style="font-size:32px;line-height:32px;">&nbsp;</td></tr></tbody></table> 
+            
+                  <center>
+                    <img width="620px" src="https://вкусняшка-иркутск.рф/css/vkusnyashka/emails/offers/newyear2021/img/main3.jpg" align="center" class="float-center">
+                  </center>
+            
+                  <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
+            
+                  <h5 class="text-center">Вкусняшка<br> Магазин новогодних подарков!</h5>
+                  <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
+                  <p>Вот уже 8 лет мы создаем сладкие подарки и хорошее настроение к Новому году!</p>
+            
+                  <p>Предприятия, родительские комитеты и главные сладкоежки – дети ценят качество наших подарков за свежесть, оригинальность упаковки и дополнительные бонусы, которые есть практически во всех наших подарках.</p>
+            
+                  <p>Все подарки фасуются вручную с контролем качества. Мы взвешиваем каждый подарок во избежание ошибок.</p>
+                  <p>Работаем только с проверенными российскими производителями конфет и шоколада!</p>
+            
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="32" style="font-size:32px;line-height:32px;">&nbsp;</td></tr></tbody></table> 
+                  <center>
+                    <h5 class="text-center float-center" align="center">Большой выбор подарков<br> в различной новогодней упаковке</h5>
+                  </center>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-4 columns first"><table><tbody><tr><th>
+                  <center>
+                    <img src="https://вкусняшка-иркутск.рф/storage/1/media/catalogs_goods_items/1/img/medium/photo_1570685330.jpg" align="center" class="float-center">
+                    <table align="center" class="spacer float-center"><tbody><tr><td height="10" style="font-size:10px;line-height:10px;">&nbsp;</td></tr></tbody></table> 
+                  </center>
+                  <h5 class="text-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/tekstilnye-meshochki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Текстильные мешочки</a></h5>
+                </th></tr></tbody></table></th>
+                <th class="small-12 large-4 columns"><table><tbody><tr><th>
+                  <center>
+                    <img src="https://вкусняшка-иркутск.рф/storage/1/media/catalogs_goods_items/2/img/medium/photo_1570685377.jpg" align="center" class="float-center">
+                    <table align="center" class="spacer float-center"><tbody><tr><td height="10" style="font-size:10px;line-height:10px;">&nbsp;</td></tr></tbody></table> 
+                  </center>
+                  <h5 class="text-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/kartonnye-upakovki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Картонные упаковки</a></h5>
+                </th></tr></tbody></table></th>
+                <th class="small-12 large-4 columns last"><table><tbody><tr><th>
+                  <center>
+                    <img src="https://вкусняшка-иркутск.рф/storage/1/media/catalogs_goods_items/3/img/medium/photo_1570685416.jpg" align="center" class="float-center">
+                    <table align="center" class="spacer float-center"><tbody><tr><td height="10" style="font-size:10px;line-height:10px;">&nbsp;</td></tr></tbody></table> 
+                  </center>
+                  <h5 class="text-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/zheleznye-banki-i-tuby/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Железные банки и тубы</a></h5>
+                </th></tr></tbody></table></th>
+              </tr></tbody></table>
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-4 columns first"><table><tbody><tr><th>
+                  <center>
+                    <img src="https://вкусняшка-иркутск.рф/storage/1/media/catalogs_goods_items/4/img/medium/photo_1570685440.jpg" align="center" class="float-center">
+                    <table align="center" class="spacer float-center"><tbody><tr><td height="10" style="font-size:10px;line-height:10px;">&nbsp;</td></tr></tbody></table> 
+                  </center>
+                  <h5 class="text-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/podarochnye-korziny/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Подарочные корзины</a></h5>
+                </th></tr></tbody></table></th>
+                <th class="small-12 large-4 columns"><table><tbody><tr><th>
+                  <center>
+                    <img src="https://вкусняшка-иркутск.рф/storage/1/media/catalogs_goods_items/5/img/medium/photo_1570685508.jpg" align="center" class="float-center">
+                    <table align="center" class="spacer float-center"><tbody><tr><td height="10" style="font-size:10px;line-height:10px;">&nbsp;</td></tr></tbody></table> 
+                  </center>
+                  <h5 class="text-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/myagkie-igrushki-ryukzaki-podushki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Мягкие игрушки</a></h5>
+                </th></tr></tbody></table></th>
+                <th class="small-12 large-4 columns last"><table><tbody><tr><th>
+                  <center>
+                    <img src="https://вкусняшка-иркутск.рф/storage/1/media/articles/989/img/medium/photo_1599033758.jpg" align="center" class="float-center">
+                    <table align="center" class="spacer float-center"><tbody><tr><td height="10" style="font-size:10px;line-height:10px;">&nbsp;</td></tr></tbody></table> 
+                  </center>
+                  <h5 class="text-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/myagkie-igrushki-ryukzaki-podushki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Рюкзаки и подушки</a></h5>
+                </th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="32" style="font-size:32px;line-height:32px;">&nbsp;</td></tr></tbody></table> 
+                  <p>В каждом подарке бесплатные билеты для детей, купоны на скидку, а также дополнительные вложения, такие как раскраски, брелоки, игрушки!</p>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="32" style="font-size:32px;line-height:32px;">&nbsp;</td></tr></tbody></table> 
+                  <center>
+                    <h5 class="text-center float-center" align="center">Выбирайте подарки<br> в нужном ценовом диапазоне:</h5>
+                  </center>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-6 columns first last"><table><tbody><tr><th>
+                  <center>
+                    <table align="center" class="menu filter-link float-center"><tbody><tr><td><table><tbody><tr>
+                      <th class="menu-item float-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020&catalogs_goods_item=&price%5Bmin%5D=100&price%5Bmax%5D=300">От 100 до 300 руб.</a></th>
+                      <th class="menu-item float-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020&catalogs_goods_item=&price%5Bmin%5D=299&price%5Bmax%5D=600">От 300 до 600 руб.</a></th>
+                      <th class="menu-item float-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020&catalogs_goods_item=&price%5Bmin%5D=600&price%5Bmax%5D=1000">От 600 до 1000 руб.</a></th>
+                      <th class="menu-item float-center"><a href="https://вкусняшка-иркутск.рф/catalogs-goods/novogodnie-podarki/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020&catalogs_goods_item=&price%5Bmin%5D=1000">Свыше 1000 руб.</a></th>
+                    </tr></tbody></table></td></tr></tbody></table>
+                  </center>
+                </th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="32" style="font-size:32px;line-height:32px;">&nbsp;</td></tr></tbody></table> 
+                  <center>
+                    <img width="620px" src="https://вкусняшка-иркутск.рф/storage/1/media/promotions/2/img/medium_1602038963.jpg" align="center" class="float-center">
+                  </center>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="32" style="font-size:32px;line-height:32px;">&nbsp;</td></tr></tbody></table> 
+                      <h5 class="text-center">Сделайте предзаказ<br> на нашем сайте!</h5>
+                  <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
+                    <table class="button medium button expand"><tbody><tr><td><table><tbody><tr><td><center><a href="https://вкусняшка-иркутск.рф/main/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020" align="center" class="float-center">Перейти на сайт</a></center></td></tr></tbody></table></td>
+<td class="expander"></td></tr></tbody></table>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                    <center>
+                      <table align="center" class="menu float-center"><tbody><tr><td><table><tbody><tr>
+                        <th class="menu-item float-center"><a href="https://vk.com/vkusnyashka_irkutsk"><img src="https://plugins.stripo.email/static/latest//assets/img/social-icons/logo-black/vk-logo-black.png" alt="VK" title="Vkontakte" width="32" height="32"></a></th>
+                        <th class="menu-item float-center"><a href="https://www.instagram.com/vkusnyashka_podarki/"><img src="https://plugins.stripo.email/static/latest//assets/img/social-icons/logo-black/instagram-logo-black.png" alt="Ig" title="Instagram" width="32" height="32"></a></th>
+                        <th class="menu-item float-center"><a href="https://www.facebook.com/vkusnyashkairkutsk"><img src="https://plugins.stripo.email/static/latest//assets/img/social-icons/logo-black/facebook-logo-black.png" alt="Fb" title="Facebook" width="32" height="32"></a></th>
+                      </tr></tbody></table></td></tr></tbody></table>
+                    </center>
+                    <center>
+                      <table align="center" class="menu float-center"><tbody><tr><td><table><tbody><tr>
+                        <th class="menu-item float-center"><a href="tel:+79646534200">8 (964) 653-42-00</a></th>
+                      </tr></tbody></table></td></tr></tbody></table>
+                    </center>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>      
+              </tr></tbody></table>     
+            
+            
+              <table class="row"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
+                  <p class="tiny-text">Вы получили это письмо, потому что являетесь зарегистрированным пользователем<br> сайта vkusnyashka.ru</p>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+            
+              <table class="row collapsed footer"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <p class="text-center copy">&copy; Магазин сладких подарков "Вкусняшка"</p>
+                  <p class="text-center unsubscribe"><a href="#">mtgk@mail.ru</a>  | <a href="https://вкусняшка-иркутск.рф/main/?utm_source=letter&utm_medium=email&utm_campaign=1|offer_1|23.10.2020">Отписаться от рассылки</a></p>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="row collapsed footer"><tbody><tr>
+                <th class="small-12 large-12 columns first last"><table><tbody><tr><th>
+                  <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
+                  <p class="last-p"></p>
+                </th>
+<th class="expander"></th></tr></tbody></table></th>
+              </tr></tbody></table>
+            
+              <table class="spacer"><tbody><tr><td height="16" style="font-size:16px;line-height:16px;">&nbsp;</td></tr></tbody></table> 
+            </td></tr></tbody></table>
+            
+          </center>
+        </td>
+      </tr>
+    </table>
+    <!-- prevent Gmail on iOS font size manipulation -->
+   <div style="display:none; white-space:nowrap; font:15px courier; line-height:0;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </div>
+  </body>
+</html>
+
