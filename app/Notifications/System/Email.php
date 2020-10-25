4c84d5b81197eec67f8e6bc14a51f1cf78276c8d
Makc_Berluskone <makc_berluskone@mail.ru>
2020-10-25 22:42:01 +0800

    Update Email.php

--- app/Notifications/System/Email.php
+++ app/Notifications/System/Email.php
@@ -36,23 +36,31 @@ class Email
     public static function sendMailingToSubscribers()
     {
 
+        $destinations = [
+            293282078,
+            228265675
+        ];
+
+        foreach ($destinations as $destination) {
+            $response = Telegram::sendMessage([
+                'chat_id' => $destination,
+                'text' => "Начинаем отправку"
+            ]);
+        }
+
         set_time_limit(0);
         \Auth::loginUsingId(4);
 
         $mailing = Mailing::with([
             'template',
-            'list.items'
-//            => function ($q) use ($subscribersWithDispatchIds) {
-//                $q->whereNotIn('entity_id', $subscribersWithDispatchIds);
-//            }
         ])
-            ->where('company_id', auth()->user()->id)
+            ->where('company_id', auth()->user()->company_id)
             ->first();
 
-        $subscribersWithDispatchIds = Subscriber::whereHas('dispatches', function ($q) {
-            $q->where('mailing_id', 1);
+        $subscribersWithDispatchIds = Subscriber::whereHas('dispatches', function ($q) use ($mailing) {
+            $q->where('mailing_id', $mailing->id);
         })
-            ->where('company_id', auth()->user()->id)
+            ->where('company_id', auth()->user()->company_id)
             ->get([
                 'id'
             ])
@@ -72,6 +80,8 @@ class Email
                 'token' => $subscriber->token
             ];
 
+//            $path = 'vkusnyashka/templates/emails/offers/newyear2021/index';
+
             Mail::send($mailing->template->path, $data, function ($message) use ($mailing, $subscriber) {
                 $message->to($subscriber->email, 'Любимому клиенту')->subject($mailing->subject);
                 $message->from(config('mail.from.address'), config('mail.from.name'));
@@ -85,10 +95,7 @@ class Email
             $count++;
         }
 
-        $destinations = [
-            293282078,
-            228265675
-        ];
+
 
         // Отправляем на каждый telegram
         foreach ($destinations as $destination) {
