fe6e27ccddd1f5b1b06d354c4dc5bd71f55fa534
Makc Berluskone <makc.berluskone@gmail.com>
2020-12-12 12:46:40 +0800

    Update CartController.php

--- app/Http/Controllers/Project/CartController.php
+++ app/Http/Controllers/Project/CartController.php
@@ -823,7 +823,7 @@ class CartController extends BaseController
 
                     $cart['prices'][$goodsItem['id']] = [
                         'count' => $goodsItem['quantity'],
-                        'price' => $goodsItem['total_catalogs_item_discount'],
+                        'price' => $goodsItem['price'],
                     ];
                 }
 
@@ -867,7 +867,7 @@ class CartController extends BaseController
 
                 foreach ($prices_goods as $price_goods) {
                     // Проверка на различие цены
-                    if ($price_goods->total_catalogs_item_discount != $prices[$price_goods->id]['price']) {
+                    if ($price_goods->price != $prices[$price_goods->id]['price']) {
                         $result['changePrice'][] = $price_goods;
                     }
 
