4d85be949e00148d12ffede0543c7033eb0e4c3f
Makc Berluskone <makc.berluskone@gmail.com>
2020-12-30 21:28:09 +0800

    Update footer.blade.php

--- resources/views/project/layouts/footers/footer.blade.php
+++ resources/views/project/layouts/footers/footer.blade.php
@@ -1,6 +1,6 @@
                     <footer class="grid-x grid-padding-x" id="footer">
                         <div class="cell small-12 medium-4 large-6 block-footer-1">
-                            <span>&copy; @isset($filial->company->birthday_company){{ $filial->company->birthday_company->format('Y') . ' -' }} @endisset {{ date("Y") }} «{{ $filial->company->legal_form->name }} {{ $filial->company->name }}»</span>
+                            <span>&copy; @isset($filial->company->foundation_date){{ $filial->company->foundation_date->format('Y') . ' -' }} @endisset {{ date("Y") }} «{{ $filial->company->legal_form->name }} {{ $filial->company->name }}»</span>
                             <br>
 
                             @isset($filial->company->prename)
