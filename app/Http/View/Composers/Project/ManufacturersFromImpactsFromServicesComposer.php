da8f17eb31cc6f80a1bf2d5226532a24eb7f2873
Makc Berluskone <makc.berluskone@gmail.com>
2021-01-14 18:22:55 +0800

    Update ManufacturersFromImpactsFromServicesComposer.php

--- app/Http/View/Composers/Project/ManufacturersFromImpactsFromServicesComposer.php
+++ app/Http/View/Composers/Project/ManufacturersFromImpactsFromServicesComposer.php
@@ -8,7 +8,7 @@ class ManufacturersFromImpactsFromServicesComposer
 {
     public function compose(View $view)
     {
-        $manufacturers = null;
+        $manufacturers = collect();
         if ($view->catalogs_services_item) {
             $pricesServices = $view->catalogs_services_item->prices;
             $pricesServices->load([
