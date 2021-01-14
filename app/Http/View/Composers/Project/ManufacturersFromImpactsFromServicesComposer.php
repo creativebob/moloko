2cae0a5240f408fc9686c5e366c9720e03f9097f
Makc Berluskone <makc.berluskone@gmail.com>
2021-01-14 18:15:54 +0800

    Update ManufacturersFromImpactsFromServicesComposer.php

--- app/Http/View/Composers/Project/ManufacturersFromImpactsFromServicesComposer.php
+++ app/Http/View/Composers/Project/ManufacturersFromImpactsFromServicesComposer.php
@@ -8,19 +8,22 @@ class ManufacturersFromImpactsFromServicesComposer
 {
     public function compose(View $view)
     {
-        $pricesServices = $view->catalogs_services_item->prices;
-        $pricesServices->load([
-            'service.process.impacts.article.manufacturer.company'
-        ]);
+        $manufacturers = collect();
+        if ($view->catalogs_services_item) {
+            $pricesServices = $view->catalogs_services_item->prices;
+            $pricesServices->load([
+                'service.process.impacts.article.manufacturer.company'
+            ]);
 
-        if ($pricesServices) {
-            $collect = [];
-            foreach($pricesServices as $priceService) {
-                foreach($priceService->service->process->impacts as $impact) {
-                    $collect[] = $impact->article->manufacturer;
+            if ($pricesServices) {
+                $collect = [];
+                foreach($pricesServices as $priceService) {
+                    foreach($priceService->service->process->impacts as $impact) {
+                        $collect[] = $impact->article->manufacturer;
+                    }
                 }
+                $manufacturers = collect($collect)->unique();
             }
-            $manufacturers = collect($collect)->unique();
         }
 //        dd($manufacturers);
 
