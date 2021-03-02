0238dc7355b5fa241dbb820ffdae70a87cde4987
Makc Berluskone <makc.berluskone@gmail.com>
2021-03-02 16:11:45 +0800

    Тест кеша

--- app/Models/Project/Process.php
+++ app/Models/Project/Process.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Process extends Model
 {
     use Publicable;
-//    use Cachable;
+    use Cachable;
     use SoftDeletes;
 
     protected $with = [
