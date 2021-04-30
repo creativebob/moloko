d6ea51df7acef2f35e7b3a6408ac145bddfe6160
Makc Berluskone <makc.berluskone@gmail.com>
2021-04-30 11:54:20 +0800

    Бек

---
+++ app/Http/Controllers/Project/ServicesFlowController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Project;
+
+use App\Models\System\Flows\ServicesFlow;
+
+class ServicesFlowController extends BaseController
+{
+    /**
+     * ServicesFlowController constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param $slug
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($slug)
+    {
+        // TODO - 30.04.21 - Костыль для переименования services_flows в tours
+        $serviceFlow = ServicesFlow::with([
+            'process' => function ($q) {
+                $q->with([
+                    'process.photo',
+                    'prices.service.process.positions' => function ($q) {
+                        $q->where('display', true);
+                    }
+                ]);
+            },
+        ])
+            ->whereHas('process', function ($q) use ($slug) {
+                $q->whereHas('process', function ($q) use ($slug) {
+                    $q->where('slug', $slug);
+                });
+            })
+            ->first();
+//        dd($serviceFlow);
+        if (empty($serviceFlow)) {
+            abort(404);
+        }
+
+        $site = $this->site;
+
+        $page = $site->pages_public
+            ->where('alias', 'services_flow')
+            ->first();
+
+        return view($site->alias . '.pages.services_flow.index', compact('site', 'page', 'serviceFlow'));
+    }
+}
---
+++ resources/views/project/composers/services_flows/section.blade.php
@@ -0,0 +1,1 @@
+@include($site->alias . '.composers.services_flows.section')
